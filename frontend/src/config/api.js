62a83023473827bbfbed7d9ad9b822e190ac4232
Lunacia <138852324+Lunac1a@users.noreply.github.com>
2026-03-10 23:22:16 +1100

    Refactor API host and port retrieval to remove default values

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-﻿const apiHost = import.meta.env.VITE_API_HOST || "127.0.0.1";
-const apiPort = import.meta.env.VITE_API_PORT || "8080";
+﻿const apiHost = import.meta.env.VITE_API_HOST;
+const apiPort = import.meta.env.VITE_API_PORT;
 
 export const API_BASE_URL = `http://${apiHost}:${apiPort}`;
